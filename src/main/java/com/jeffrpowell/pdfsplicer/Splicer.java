45ecaccfb58326aa139e0566bd4e68aed97467f7
jeffrpowell <jeffr.powell@outlook.com>
2023-04-10 21:29:43 -0600

    Custom code to generate a specific cover page spec for B&N book upload feature

--- src/main/java/com/jeffrpowell/pdfsplicer/Splicer.java
+++ src/main/java/com/jeffrpowell/pdfsplicer/Splicer.java
@@ -3,10 +3,10 @@ package com.jeffrpowell.pdfsplicer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import javax.swing.SwingWorker;
-import org.apache.pdfbox.multipdf.Splitter;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 
 public class Splicer extends SwingWorker<Path, Void>{
 	private final Path pathToSplit;
@@ -27,16 +27,27 @@ public class Splicer extends SwingWorker<Path, Void>{
         }
 		//Split pdfs
 		try (PDDocument document = PDDocument.load(Files.readAllBytes(pathToSplit))) {
-			Splitter splitter = new Splitter();
-			List<PDDocument> splittedDocuments = splitter.split(document);
-			
-			for (int i = 1; i <= splittedDocuments.size(); i++)
+//			Splitter splitter = new Splitter();
+//			List<PDDocument> splittedDocuments = splitter.split(document);
+//			
+//			for (int i = 1; i <= splittedDocuments.size(); i++)
+//			{
+//				Path pagePath = Paths.get(splitDirectory.toString(), splitDirectoryName + "_" + i + ".pdf");
+//				try (PDDocument doc = splittedDocuments.get(i-1))
+//				{
+//					doc.save(pagePath.toFile());
+//				}
+//			}
+			PDPage page = document.getPage(0);
+			PDRectangle rect = new PDRectangle(PDRectangle.LETTER.getUpperRightX() + 9F, PDRectangle.LETTER.getUpperRightY() + 18F);
+			page.setMediaBox(rect);
+			page.setCropBox(rect);
+			page.setBleedBox(rect);
+			Path newPath = pathToSplit.resolveSibling(pathToSplit.getFileName().toString().replace(".pdf", "") + "_upload_cover.pdf");
+			try (PDDocument newDoc = new PDDocument())
 			{
-				Path pagePath = Paths.get(splitDirectory.toString(), splitDirectoryName + "_" + i + ".pdf");
-				try (PDDocument doc = splittedDocuments.get(i-1))
-				{
-					doc.save(pagePath.toFile());
-				}
+				newDoc.addPage(page);
+				newDoc.save(newPath.toFile());
 			}
 		}
 		return splitDirectory;
